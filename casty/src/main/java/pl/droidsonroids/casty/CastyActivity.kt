28751761e0ae9392a357067d028d177ab41556b8
Kaaveh <Kaaveh@pm.me>
2022-07-24 14:12:17 +0430

    [refactor] port CastyActivity to kotlin

---
+++ casty/src/main/java/pl/droidsonroids/casty/CastyActivity.kt
@@ -0,0 +1,34 @@
+package pl.droidsonroids.casty
+
+import androidx.appcompat.app.AppCompatActivity
+import pl.droidsonroids.casty.Casty
+import androidx.annotation.CallSuper
+import android.os.Bundle
+import android.view.Menu
+import android.view.View
+import pl.droidsonroids.casty.R
+
+/**
+ * Extensible [AppCompatActivity], which helps with setting widgets
+ */
+abstract class CastyActivity : AppCompatActivity() {
+
+    protected lateinit var casty: Casty
+
+    @CallSuper
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        casty = Casty.create(this)
+    }
+
+    @CallSuper
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        super.onCreateOptionsMenu(menu)
+        if (findViewById<View?>(R.id.casty_mini_controller) == null) {
+            casty.addMiniController()
+        }
+        casty.addMediaRouteMenuItem(menu)
+        return true
+    }
+
+}
